1351e2152a728c1d871d1b8ad40adf00fcb5cb6e
Ricky Thonglyvong <rthonglyvong@gmail.com>
2025-01-04 21:34:39 -0800

    fixed centering on cover and added some images

--- YorushikaVue/src/App.vue
+++ YorushikaVue/src/App.vue
@@ -7,15 +7,17 @@
     <div id="pageflip-container" style="background-image: url('/background.jpg'); background-size: cover; background-position: center;">
       <!-- First Cover Page -->
       <div class="page page-cover" data-density="hard">
-        <h2>{{ pages[0].content.title }}</h2>
-        <p>{{ pages[0].content.body }}</p>
+        <div class="page-content">
+          <h2>{{ pages[0].content.title }}</h2>
+          <p>{{ pages[0].content.body }}</p>
+        </div>
       </div>
 
       <!-- Dynamic Pages -->
       <div v-for="(page, index) in pages.slice(1, pages.length - 1)" :key="index" class="page" :data-type="page.type">
         <div class="page-content">
           <h2>{{ page.content.title }}</h2>
-          <p v-if="page.type === 'Text'">{{ page.content.body }}</p>
+          <p v-if="page.type === 'Text'" v-html="convertNewlines(page.content.body)"></p>
           <iframe v-if="page.type === 'Video'" :src="page.content.body" width="100%" height="100%" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
           <img v-if="page.type === 'Image'" :src="page.content.body" alt="Page image" style="max-width:100%; max-height:100%; object-fit:contain;">
         </div>
@@ -23,8 +25,10 @@
 
       <!-- Last Cover Page -->
       <div class="page page-cover" data-density="hard">
-        <h2>{{ pages[pages.length - 1].content.title }}</h2>
-        <p>{{ pages[pages.length - 1].content.body }}</p>
+        <div class="page-content">
+          <h2>{{ pages[pages.length - 1].content.title }}</h2>
+          <p>{{ pages[pages.length - 1].content.body }}</p>
+        </div>
       </div>
     </div>
     <div class="controls">
@@ -73,6 +77,9 @@ export default {
     });
   },
   methods: {
+    convertNewlines(text) {
+      return text.replace(/\n/g, '<br>');
+    },
     // Preload all BGM files
     preloadBgm() {
       this.pages.forEach((page, index) => {
